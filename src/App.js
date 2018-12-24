56a32859f42c581b96c85fe515dbb7b33304084c
Vaselek <burzhuas@gmail.com>
2018-12-24 16:11:40 +0600

    Add pocker game

--- src/App.js
+++ src/App.js
@@ -1,28 +1,50 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Card from './components/Card/Card';
+import CardDeck from './components/CardDeck/CardDeck';
+import PokerHand from './components/PokerHand/PokerHand';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+
+  state = {
+    deck: [],
+    cards: [],
+    outcome: []
+  };
+
+  startGame() {
+    const deck = new CardDeck();
+    const cards = deck.getCards(5);
+    const pokerHand = new PokerHand(cards);
+    const outcome = pokerHand.getOutcome();
+    this.setState({deck});
+    this.setState({cards});
+    this.setState({outcome})
   }
+
+  render() {
+      const cards = this.state.cards.map((card, index) => {
+        return <Card key={index}
+                     rank={card.rank}
+                     suit={card.suit} />
+      });
+      const outcome = this.state.outcome.map((msg) => {
+          return <div>
+            <div>Combo: {msg.msg}</div>
+            <div>Rank {msg.rank}</div>
+          </div>
+      })
+
+      return (
+        <div className="App">
+            <button type='submit' onClick={()=>this.startGame()}>Shuffle cards</button>
+            <div className="cards">
+                {cards}
+            </div>
+            {this.state.outcome.length > 0 && outcome}
+        </div>
+      );
+    }
 }
 
 export default App;
